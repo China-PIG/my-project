1cf7307dd6068b8e790cef45a8cca50bd9a5c7c6
China-PIG <1664714531@qq.com>
2025-08-06 10:53:14 +0800

    4th commit

---
+++ work2/Core/Src/main.c
@@ -0,0 +1,268 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "dma.h"
+#include "tim.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+uint8_t currentAngle = 0; 
+#define RX_LEN 7
+uint8_t RxData[RX_LEN];
+void Servo_SetAngle(uint8_t angle);
+char msg[20];
+
+#define TX_BUF_LEN 20
+uint8_t TxData[TX_BUF_LEN];
+uint8_t uart_tx_busy = 0;  // 串口DMA是否忙
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_USART1_UART_Init();
+  MX_TIM2_Init();
+  /* USER CODE BEGIN 2 */
+	HAL_UART_Receive_DMA(&huart1, RxData, RX_LEN);
+	HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+		/*HAL_UART_Transmit_DMA(&huart1, TxData, 5);
+		HAL_Delay(1000);
+		Servo_SetAngle(0);  
+		HAL_Delay(1000);
+		Servo_SetAngle(90); 
+		HAL_Delay(1000);
+		Servo_SetAngle(180); 
+		HAL_Delay(1000);*/
+     if (uart_tx_busy == 0)
+    {
+        int len = sprintf((char*)TxData, "ANGLE:%d\r\n", currentAngle);
+        uart_tx_busy = 1;  // 设置为忙，等发送完成后再清0
+        HAL_UART_Transmit_DMA(&huart1, TxData, len);
+    }
+
+    HAL_Delay(100);  // 每100ms尝试发送
+		
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+	void Servo_SetAngle(uint8_t angle)
+	{
+    if (angle > 180) angle = 180;
+    uint16_t pulse = 500 + ((2000 * angle) / 180);
+    __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, pulse);
+		currentAngle = angle; 
+	}
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+    if (huart == &huart1)
+    {
+        if (RxData[0] == 'A' && RxData[1] == 'N' && RxData[2] == 'G' && RxData[3] == ':')
+        {
+            char angleStr[4] = {0};  // 最多3位数字 + 结束符
+
+
+            int idx = 0;
+            for (int i = 4; i < RX_LEN; i++)
+            {
+                if (RxData[i] >= '0' && RxData[i] <= '9' && idx < 4)  
+                {
+                    angleStr[idx++] = RxData[i];
+                }
+                else
+                {
+                    break;
+                }
+            }
+            angleStr[idx] = '\0';  // 结尾
+
+            int angle = atoi(angleStr);
+
+            if (angle >= 0 && angle <= 180)
+            {
+                Servo_SetAngle((uint8_t)angle);
+                HAL_UART_Transmit(&huart1, (uint8_t*)"CMD OK\r\n", 8, 100);
+            }
+            else
+            {
+                HAL_UART_Transmit(&huart1, (uint8_t*)"ERR ANG\r\n", 9, 100);
+            }
+        }
+        else
+        {
+            HAL_UART_Transmit(&huart1, (uint8_t*)"UNK CMD\r\n", 10, 100);
+        }
+
+        HAL_UART_Receive_DMA(&huart1, RxData, 8);  // 重新启动接收
+    }
+}
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart)
+{
+    if (huart == &huart1)
+    {
+        uart_tx_busy = 0;  // 发送完成，允许下一次发送
+    }
+}
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
